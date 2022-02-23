97fe8900e1e6a5ef19d18aa53cbb1f2be71d25b2
msolano <msolano@imaginacolombia.com>
2022-02-23 17:43:03 -0500

    +Add: config `cmsPages`

---
+++ Config/cmsPages.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+  'admin' => [
+    "documents" => [
+      "permission" => "idocs.documents.manage",
+      "activated" => true,
+      "authenticated" => true,
+      "path" => "/document/documents",
+      "name" => "qdocument.admin.documents",
+      "crud" => "qdocument/_crud/documents",
+      "page" => "qcrud/_pages/admin/crudPage",
+      "layout" => "qsite/_layouts/master.vue",
+      "title" => "idocs.cms.sidebar.adminDocuments",
+      "icon" => "fas fa-folder-open",
+      "subHeader" => [
+        "refresh" => true
+      ]
+    ],
+    "categories" => [
+      "permission" => "idocs.categories.manage",
+      "activated" => true,
+      "authenticated" => true,
+      "path" => "/document/categories",
+      "name" => "qdocument.admin.categories",
+      "crud" => "qdocument/_crud/categories",
+      "page" => "qcrud/_pages/admin/crudPage",
+      "layout" => "qsite/_layouts/master.vue",
+      "title" => "idocs.cms.sidebar.adminCategories",
+      "icon" => "fas fa-layer-group",
+      "subHeader" => [
+        "refresh" => true
+      ]
+    ]
+  ],
+  'panel' => [
+    "documents" => [
+      "permission" => "idocs.documents.manage",
+      "activated" => true,
+      "authenticated" => true,
+      "path" => "/document/documents",
+      "name" => "qdocument.panel.documents",
+      "crud" => "qdocument/_crud/documents",
+      "page" => "qcrud/_pages/admin/crudPage",
+      "layout" => "qsite/_layouts/master",
+      "title" => "idocs.cms.sidebar.panelDocuments",
+      "icon" => "fas fa-folder-open",
+      "subHeader" => [
+        "refresh" => true
+      ]
+    ]
+  ],
+  'main' => []
+];
