7a54f345552556f43d4335d13d852df3e3f4ac3b
msolano <msolano@imaginacolombia.com>
2022-03-01 19:22:55 -0500

    +Update: CMSSidebar config

--- Config/cmsSidebar.php
+++ Config/cmsSidebar.php
@@ -6,12 +6,12 @@ return [
       "title" => "idocs.cms.sidebar.adminGroup",
       "icon" => "fas fa-folder-open",
       "children" => [
-        "idocs_cms_admin_documents",
+        "idocs_cms_main_documents",
         "idocs_cms_admin_categories",
       ]
     ]
   ],
   "panel" => [
-    "idocs_cms_panel_documents"
+    "idocs_cms_main_documents"
   ],
 ];
