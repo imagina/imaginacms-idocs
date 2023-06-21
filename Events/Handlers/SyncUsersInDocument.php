fc216dbcc77a58f26feec984c5e528906f6745d8
Cristian Marin <marin101998@gmail.com>
2023-06-21 15:13:14 -0500

    +fix | sync users documents

--- Events/Handlers/SyncUsersInDocument.php
+++ Events/Handlers/SyncUsersInDocument.php
@@ -12,36 +12,42 @@ use Illuminate\Support\Arr;
 
 class SyncUsersInDocument
 {
-  
 
-    public function __construct()
-    {
-    
+
+  public function __construct()
+  {
+
+  }
+
+  public function handle($event)
+  {
+    try {
+
+    $document = $event->document;
+    $data = $event->data;
+    $users = Arr::get($data, 'users', []);
+
+    foreach ($users as $userId) {
+      $userDocument = DocumentUser::where("document_id", $document->id)->where("user_id", $userId)->first();
+      if (!isset($userDocument->id)) {
+        DocumentUser::create([
+          "document_id" => $document->id,
+          "user_id" => $userId,
+          "key" => null,
+        ]);
+      }
     }
 
-    public function handle($event)
-    {
-        try {
-        
-          $document = $event->document;
-          $data = $event->data;
-          $users = Arr::get($data, 'users', []);
-    
-         foreach ($users as $userId){
-           $userDocument = DocumentUser::where("document_id", $document->id)->where("user_id",$userId)->first();
-           
-           if(!isset($userDocument->id)){
-             DocumentUser::create([
-               "document_id" => $document->id,
-               "user_id" => $userId,
-               "key" => null,
-             ]);
-           }
-         }
-          
-        } catch (\Exception $e) {
-            \Log::error($e->getMessage());
-            return $e;
-        }
+    $usersDocument = DocumentUser::where("document_id", $document->id)->get();
+    foreach ($usersDocument as $userDocument) {
+      if(!in_array($userDocument->user_id, $users)) {
+        $userDocument->delete();
+      }
     }
+
+    } catch (\Exception $e) {
+      \Log::error($e->getMessage());
+      return $e;
+    }
+  }
 }
