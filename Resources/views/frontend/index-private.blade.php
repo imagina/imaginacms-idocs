8bf4b4765a27add058928e5a4f771c8c3cf27c92
Cristian Marin <marin101998@gmail.com>
2025-08-06 10:08:32 -0500

    + fix | v10 06-08-25 styles module documents public and private view

--- Resources/views/frontend/index-private.blade.php
+++ Resources/views/frontend/index-private.blade.php
@@ -18,25 +18,54 @@
 
 @section('content')
   <div id="privateDocumentsAll">
-    <x-isite::breadcrumb>
-      <li class="breadcrumb-item active" aria-current="page"> {{trans('idocs::frontend.privateDocuments')}}</li>
-    </x-isite::breadcrumb>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <!--Translation  Title _ idocs::common.idocs.title -->
-          <h1 class="docs-title h3">{{isset($category->id)
-                         ? $category->title
-                         : trans("idocs::common.title.idocs")}}
-          </h1>
-
-          <!-- Translation Description _ idocs::common.idocs.description -->
-          <p class="docs-description">{!! isset($category->id)
-                       ? $category->description
-                        : trans("idocs::common.description.idocs") !!}
-          </p>
+    <div class="breadcrumb-section">
+      <div class="content overflow-hidden">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-12 ">
+            <div class="breadcrumb-content position-relative overlay-hidden  order-0 ">
+              <div class="breadcrumb-info mx-3 mx-lg-5 breadcrumb-overlay">
+                <div class="h-100 container ">
+                  <div class="h-100 row align-items-center">
+                    <div class="col-auto">
+                      <div class="title-section  text-white ">
+                        {{ trans("idocs::common.title.idocs")}}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="b-image">
+                @if(isset($category) && $category->mediaFiles() && $category->mediaFiles()->mainimage && !Str::contains($category->mediaFiles()->mainimage->path, 'default'))
+                  <x-media::single-image
+                    imgClasses=""
+                    :mediaFiles="$category->mediaFiles()"
+                    :isMedia="true"
+                    :alt="$category->title"
+                  />
+                @else
+                  <x-media::single-image
+                    imgClasses=""
+                    setting="icustom::breadcrumbDocuments"
+                    alt="img-media-breadcrumb"/>
+                @endif
+              </div>
+              @if(isset($category))
+                <div class=" breadcrumb-content h3 text-center py-3">{{$category->title}}</div>
+              @endif
+            </div>
+            <div class="breadcrumb-tree order-1">
+              <x-isite::breadcrumb>
+                <li class="breadcrumb-item active"
+                    aria-current="page"> {{trans('idocs::frontend.publicDocuments')}}</li>
+              </x-isite::breadcrumb>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+
+    <div id="contentListDocumentsAll">
+      <div class="container">
         @if(isset($category))
           <div class="col-12">
             @if(isset($category->id))
